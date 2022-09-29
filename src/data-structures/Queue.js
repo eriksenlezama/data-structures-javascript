5ae09e52dc8dc35a33cc43b13c726c25e3b86414
Eriksen Lezama <eriksen.lezama@gmail.com>
2022-09-29 14:04:43 -0500

    Queue data structure

---
+++ src/data-structures/Queue.js
@@ -0,0 +1,74 @@
+class Node {
+  constructor(value, next = null) {
+    this.value = value
+    this.next = next
+  }
+}
+
+const Queue = (function() {
+  const queue = {}
+
+  return class {
+    constructor() {
+      queue.head = null
+      queue.tail = null
+      queue.size = 0
+    }
+
+    enqueue(value) {
+      const newNode = new Node(value)
+
+      queue.size += 1
+
+      if (!queue.head) {
+        queue.head = newNode
+        queue.tail = newNode
+
+        return queue
+      }
+
+      queue.tail.next = newNode
+      queue.tail = newNode
+
+      return queue
+    }
+
+    dequeue() {
+      if (!queue.head) return
+
+      const head = queue.head
+
+      if (queue.head.next) {
+        queue.head = queue.head.next
+      } else {
+        queue.head = null
+        queue.tail = null
+      }
+
+      queue.size -= 1
+
+      return head
+    }
+
+    toArray() {
+      const array = []
+
+      if (!queue.head) return array
+
+      let currentNode = queue.head
+
+      while (currentNode) {
+        array.push(currentNode.value)
+        currentNode = currentNode.next
+      }
+
+      return array
+    }
+
+    toString() {
+      return this.toArray().toString()
+    }
+  }
+})()
+
+const queue = new Queue()
