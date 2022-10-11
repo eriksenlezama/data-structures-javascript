dbe673c9f9257ff2fe40ba426f4509ad83eaa233
Eriksen Lezama <eriksen.lezama@gmail.com>
2022-10-10 21:00:23 -0500

    Add insert and find methods

--- src/data-structures/Trie.js
+++ src/data-structures/Trie.js
@@ -24,5 +24,37 @@ export const Trie = (function () {
     constructor() {
       trie.root = new Node()
     }
+
+    insert(value, node = trie.root, parent = null) {
+      if (typeof value !== 'string') {
+        return null
+      }
+
+      node.parent = parent
+
+      if (value.length === 0) {
+        node.setTerminal()
+        return trie
+      }
+
+      if (!node.children.has(value.charAt(0))) {
+        node.children.set(value.charAt(0), new Node())
+        return this.insert(value.substring(1), node.children.get(value.charAt(0)), node)
+      } else {
+        return this.insert(value.substring(1), node.children.get(value.charAt(0)), node)
+      }
+    }
+
+    find(value, node = trie.root) {
+      if (node.children.has(value.charAt(0))) {
+        if (value.length === 1 && node.children.get(value.charAt(0)).isTerminal()) {
+          return true
+        } else {
+          return this.find(value.substring(1), node.children.get(value.charAt(0)))
+        }
+      } else {
+        return false
+      }
+    }
   }
 })()
